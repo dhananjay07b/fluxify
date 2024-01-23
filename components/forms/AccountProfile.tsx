af439146739d1702b4d18a9790107dc3176ced54
Dhananjay <dhananjaybhardwaj.07@gmail.com>
2024-01-23 11:22:52 +0530

    uploadThing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -14,12 +14,14 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea";
-
+import { useUploadThing } from '@/lib/uploadthing'
 import * as z from "zod"
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import { isBase64Image } from "@/lib/utils";
 
 interface Props {
     user: {
@@ -35,6 +37,9 @@ interface Props {
 
 const AccountProfile = ({user, btnTitle} : Props) => {
     
+    const [files, setFiles] = useState<File[]>([])
+    const {startUpload} = useUploadThing("media");
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues:{
@@ -45,14 +50,39 @@ const AccountProfile = ({user, btnTitle} : Props) => {
         }
     })
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if(e.target.files && e.target.files.length > 0){
+            const file = e.target.files[0];
+
+            setFiles(Array.from(e.target.files));
+
+            if(!file.type.includes('image')) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || '';
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file);
+        }
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo;
+        const hasImageChanged = isBase64Image(blob);
+
+        if(hasImageChanged){
+            const imgRes = await startUpload(files)
+            if(imgRes && imgRes[0].url) {
+                values.profile_photo = imgRes[0].url;
+            }
+        }
+
+        //TODO: Update User Profile
     }
 
 
